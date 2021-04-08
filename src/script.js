04db33510bdf8e8f9e19527e754f1bb392a2490d
leandroandrade1 <lelisiak@gmail.com>
2021-04-08 14:49:11 -0300

    configurando o slide no arquivo html e javaccript

---
+++ src/script.js
@@ -0,0 +1,11 @@
+
+$(document).ready(() => {
+    const slickOptions = {
+      infinite: true,
+      autoplay: true, 
+      autoplaySpeed: 3000,
+      dots: false,   
+    };
+    $(".slider").slick(slickOptions){
+    
+  });
