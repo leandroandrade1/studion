319d6344c62d924ad10fae8e3f1cf7d7b6bdf035
leandroandrade1 <lelisiak@gmail.com>
2021-04-22 10:39:17 -0300

    finalizando a pagina studion

--- src/script.js
+++ src/script.js
@@ -1,8 +1,33 @@
 $('.slider') .slick({
   infinite: true,
+  autoplay: true,
   speed: 50,
   fade: true,
   cssEase: 'linear',
-  dots: false
+  dots: false,
+  arrows: true,
+  prevArrow: '<button type="button" class="slick-prev slider__prev-arrow ">Previous</button>;',
+  nextArrow: '<button type="button" class="slick-next slider__next-arrow ">Previous</button>;'
+
+  
+
+  
+});
+
+$('.footer__form-button').on('click',() =>{
+  const email = $('#.email').val();
+  Email.send({
+    Host : "smtp.gmail.com",
+    Username : "username",
+    Password : "passord",
+    To : 'them@website.com',
+    From : email,
+    Subject : "assunto do email",
+    Body : "texto do email"
+}).then(
+  message => alert(message)
+);
+
+
 });
 		
